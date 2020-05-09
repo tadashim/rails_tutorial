898f53718d3d272bbd2405116b00bf97cae4b945
tadashim <ma.tadashi@gmail.com>
2020-05-09 22:00:50 +0900

    Add forget method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -28,7 +28,15 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
