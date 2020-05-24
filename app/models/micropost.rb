6e4dd35c66d8f42f3e5f4734c38aab2195903d8d
tadashim <ma.tadashi@gmail.com>
2020-05-25 01:30:31 +0900

    Add user microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,14 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
+
+  private
+
+    # アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 1.megabytes
+        errors.add(:picture, "should be less than 1MB")
+      end
+    end
 end
